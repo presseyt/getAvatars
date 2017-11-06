5190aff93599faec52990430bb6c7ec286c819b6
Timothy Pressey <timothy.pressey@gmail.com>
2017-11-06 19:25:28 +0000

    downloaded contributors with http using token authentication

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,38 @@
 
 const request = require('request');
+const GITHUB_TOKEN = require('./secret.js').GITHUB_TOKEN;
 
 function getRepoContributors(repoOwner, repoName, cb){
 
+  var requestOptions = {
+    uri: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'request',
+      'AUTHORIZATION': 'token ' + GITHUB_TOKEN
+    }
+
+  };
+
+  request(requestOptions, function(err, req, body){
+    cb(err, body);
+  });
 
 
+
+  // request.get(requestOptions)
+  //        .on('error', function(err){
+  //           throw err;
+  //        })
+  //        .on('response', function(res){
+  //          console.log(res.statusCode);
+  //          return res;
+  //        });
 }
+
+
+function printResults(err, body){
+  console.log('Errors: ', err);
+  console.log('Body: ', body);
+}
+
+getRepoContributors('jquery', 'jquery', printResults);
