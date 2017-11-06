0ff312e753cf0a7a7d16c76fa16ed61ce618aa4e
Timothy Pressey <timothy.pressey@gmail.com>
2017-11-06 19:45:56 +0000

    made a function that prints all the URLs for the avatar of each contributor

--- download_avatars.js
+++ download_avatars.js
@@ -2,37 +2,37 @@
 const request = require('request');
 const GITHUB_TOKEN = require('./secret.js').GITHUB_TOKEN;
 
-function getRepoContributors(repoOwner, repoName, cb){
 
+//gets the github list of contributors for the given repository owner + name
+//calls the cb function once has recieved the api body
+function getRepoContributors(repoOwner, repoName, cb){
   var requestOptions = {
     uri: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
       'User-Agent': 'request',
       'AUTHORIZATION': 'token ' + GITHUB_TOKEN
     }
-
   };
 
   request(requestOptions, function(err, req, body){
     cb(err, body);
   });
-
-
-
-  // request.get(requestOptions)
-  //        .on('error', function(err){
-  //           throw err;
-  //        })
-  //        .on('response', function(res){
-  //          console.log(res.statusCode);
-  //          return res;
-  //        });
 }
 
 
+//a simple function to output the body
 function printResults(err, body){
   console.log('Errors: ', err);
   console.log('Body: ', body);
 }
 
-getRepoContributors('jquery', 'jquery', printResults);
+//prints to the screen a list of contributors and the URLs of their avatars
+function printAvatarURLs(err, body){
+  if (err) throw err;
+
+  JSON.parse(body).forEach(function(contributor){
+    console.log(contributor.login, ':', ' '.repeat(15 - contributor.login.length), contributor.avatar_url);
+  });
+}
+
+getRepoContributors('jquery', 'jquery', printAvatarURLs);
